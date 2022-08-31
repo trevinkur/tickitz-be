b1b0f1499e89787cd2c442dd440482fb746e5801
trevinkur <mokeyprojek@gmail.com>
2022-09-01 03:44:15 +0700

    fix bug

--- controller/notifController.js
+++ controller/notifController.js
@@ -11,7 +11,8 @@ module.exports = {
                     credential: admin.credential.cert({
                         client_email: process.env.FIREBASE_CLIENT_EMAIL,
                         private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-                        project_id: process.env.FIREBASE_PROJECT_ID
+                        project_id: process.env.FIREBASE_PROJECT_ID,
+                        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID
                     })
                 })
             }
